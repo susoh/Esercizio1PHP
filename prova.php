649c184ac4bb54dc812e0080337d68e2dc082775
Davide Susini <susini.davide@itismeucci.com>
2024-09-20 09:35:46 +0200

    creazione della rep git

---
+++ prova.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Susini Davide</h1>
+    <?php 
+        $peso = 80;
+        $tasso = $peso * 30;
+        echo "<p> il tasso di idrazione per un persona che pesa: $peso è di $tasso ml";
+    ?>
+</body>
+</html>
